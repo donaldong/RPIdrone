9ad04e833366964ad071249e730e8f8bc785ca9d
donaldong <xdong@csumb.edu>
2017-12-10 06:44:25 +0000

    MPU moving avg

--- mpu/mpu.py
+++ mpu/mpu.py
@@ -1,4 +1,6 @@
 from mpu6050 import mpu6050
+from collections import deque
+import copy
 
 class MPU:
     ADDRESS = 0x68
@@ -6,24 +8,54 @@ class MPU:
     CALIBRATION = 100
     
     def __init__(self):
-        self.__gyro_offset = self.ZERO
-        self.__gyro_count = 0
-        self.__accel_offset = self.ZERO
-        self.__accel_count = 0
+        self.__gyro = deque()
+        self.__accel = deque()
         self.__sensor = mpu6050(self.ADDRESS)
 
     def gyro(self):
         data = self.__sensor.get_gyro_data() 
-        if self.__gyro_count < self.CALIBRATION:
-            self.__gyro_offset["x"] += data["x"]
-            self.__gyro_offset["y"] += data["y"]
-            self.__gyro_offset["z"] += data["z"]
-            self.__gyro_count += 1
-            return self.ZERO
-        data["x"] -= self.__gyro_offset["x"] / self.CALIBRATION
-        data["y"] -= self.__gyro_offset["y"] / self.CALIBRATION
-        data["z"] -= self.__gyro_offset["z"] / self.CALIBRATION
+        if len(self.__gyro) < self.CALIBRATION:
+            self.__gyro.append(data)
+            return self.__average(self.__gyro)
+        avg = self.__average(self.__gyro)
+        self.__gyro.popleft()
+        self.__gyro.append(copy.deepcopy(data))
+        self.__sub(data, avg)
         return data
 
     def accel(self):
-        return self.__sensor.get_accel_data()
+        data = self.__sensor.get_accel_data()
+        if len(self.__accel) < self.CALIBRATION:
+            self.__accel.append(data)
+            return self.__average(self.__accel)
+        avg = self.__average(self.__accel)
+        self.__accel.popleft()
+        self.__accel.append(copy.deepcopy(data))
+        self.__sub(data, avg)
+        return data
+
+    @staticmethod
+    def __zero():
+        return copy.deepcopy(MPU.ZERO)
+
+    @staticmethod
+    def __add(a, b):
+        a["x"] += b["x"]
+        a["y"] += b["y"]
+        a["z"] += b["z"]
+
+    @staticmethod
+    def __sub(a, b):
+        a["x"] -= b["x"]
+        a["y"] -= b["y"]
+        a["z"] -= b["z"]
+
+    def __average(self, data):
+        res = self.__zero()
+        for d in data:
+            self.__add(res, d)
+        res["x"] /= len(data)
+        res["y"] /= len(data)
+        res["z"] /= len(data)
+        return res
+
