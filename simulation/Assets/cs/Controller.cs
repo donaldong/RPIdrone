0f48c9f80cfa40d12c562a782972ca8e2d40ea92
Donald Dong <xdong@csumb.edu>
2017-12-10 21:40:03 -0800

    Reset when an episode finishes

--- simulation/Assets/cs/Controller.cs
+++ simulation/Assets/cs/Controller.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,10 +11,8 @@ class Reward {
 		this.value = value;
 	}
 
-	public Reward(Vector3 eulerAngle) {
-		var up = new Vector3 (0, 1, 0);
-		var diff = up - eulerAngle;
-		this.value = -diff.magnitude;
+	public Reward(Vector3 diff, Vector3 accel) {
+		this.value = -diff.magnitude * 2 -  accel.magnitude;
 	}
 
 	public float getValue() {
@@ -21,13 +20,44 @@ class Reward {
 	}
 }
 
-class State {
+class State : IEquatable<State> {
 	public Vector3 gyro;
 	public Vector3 accel;
 
 	public State(Vector3 gyro, Vector3 accel) {
 		this.gyro = gyro;
 		this.accel = accel;
+	}
+
+	public int getHashGyro() {
+		int res = Convert.ToInt32(gyro.x / 6.0f);
+		res <<= 6;
+		res += Convert.ToInt32(gyro.y / 6.0f);
+		res <<= 6;
+		res += Convert.ToInt32(gyro.z / 6.0f);
+		return res;
+	}
+
+	public int getHashAccel() {
+		int res = Convert.ToInt32(accel.x / 7.0f);
+		res <<= 4;
+		res += Convert.ToInt32(accel.y / 7.0f);
+		res <<= 4;
+		res += Convert.ToInt32(accel.z / 7.0f);
+		return res;
+	}
+
+	public override int GetHashCode() {
+		int gyroHash = getHashGyro ();
+		return gyroHash << 18 + getHashAccel();
+	}
+
+	public override bool Equals(object obj) {
+		return Equals(obj as State);
+	}
+
+	public bool Equals(State state) {
+		return state != null && state.GetHashCode() == this.GetHashCode();
 	}
 }
 
@@ -78,7 +108,8 @@ class Motor {
 
 	public void reset() {
 		percent = 1.0f;
-		obj.GetComponent<Rigidbody> ().velocity = new Vector3 (0, 0, 0);
+		obj.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+		obj.GetComponent<Rigidbody> ().angularVelocity = Vector3.zero;
 	}
 }
 
@@ -98,7 +129,7 @@ class Environment {
 		this.b2 = new Motor(b2);
 		observation = new Observation (
 			new Reward(0),
-			new State (start_rot, new Vector3 (0, 0, 0)),
+			new State (start_rot, Vector3.zero),
 			false
 		);
 		start_pos = drone.transform.position;
@@ -151,7 +182,8 @@ class Environment {
 	public void reset() {
 		drone.transform.position = start_pos;
 		drone.transform.eulerAngles = start_rot;
-		drone.GetComponent<Rigidbody> ().velocity = new Vector3 (0, 0, 0);
+		drone.GetComponent<Rigidbody> ().velocity = Vector3.zero;
+		drone.GetComponent<Rigidbody> ().angularVelocity = Vector3.zero;
 		r1.reset ();
 		r2.reset ();
 		b1.reset ();
@@ -165,17 +197,36 @@ class Environment {
 	public Action getLastAction() {
 		return last;
 	}
+
+	public static Vector3 getDiff(Transform t) {
+		return t.rotation * Vector3.up - Vector3.up;
+	}
+
+	public bool end() {
+		var diff = getDiff (drone.transform);
+		if (diff.magnitude > 0.5)
+			return true;
+		if (drone.transform.position.y >= 10.0f)
+			return true;
+		if (drone.transform.position.x >= 150.0f || drone.transform.position.x <= -150.0f)
+			return true;
+		if (drone.transform.position.z >= 150.0f || drone.transform.position.z <= -150.0f)
+			return true;
+		return false;
+	}
 }
 
 public class Controller : MonoBehaviour {
 	public GameObject drone;
 	public GameObject r1, r2, b1, b2;
 	public float max_thrust = 0.6f;
-    public static float learningRate = 0.5f;
-    public static float epsilon = 0.2f;
-    public static float discountFactor = 0.9f;
-	public Text gyroText, accelText;
-
+    public float learningRate = 0.5f;
+    public float epsilon = 0.2f;
+    public float discountFactor = 0.9f;
+	public int maxEpisodeLength = 1000000;
+	public Text gyroText, accelText, episodeText;
+	private int step_count = 0;
+	private int episode = 1;
 	private float step = 0.1f;
 	private float[] thrust; // in grams
 	private Environment environment;
@@ -194,8 +245,11 @@ public class Controller : MonoBehaviour {
 		Update (Input.GetKey (KeyCode.S), b1, 1);
 		Update (Input.GetKey (KeyCode.D), r2, 2);
 		Update (Input.GetKey (KeyCode.F), b2, 3);
-		if (Input.GetKeyDown (KeyCode.R))
+		if (Input.GetKeyDown (KeyCode.R) || environment.end () || step_count > maxEpisodeLength) {
 			reset ();
+			episode += 1;
+			step_count = 0;
+		}
 		environment.step (Policy.epsilon_greedy_policy (
 			environment.observe().state,
 			epsilon
@@ -207,7 +261,7 @@ public class Controller : MonoBehaviour {
 		Vector3 acceleration = (rigidbody.velocity - lastVelocity) / Time.fixedDeltaTime;
 		lastVelocity = rigidbody.velocity;
 		Observation observation = new Observation (
-			new Reward(transform.eulerAngles),
+			new Reward(Environment.getDiff(transform), acceleration),
 			new State(transform.eulerAngles, acceleration),
 			false
 		);
@@ -215,6 +269,10 @@ public class Controller : MonoBehaviour {
 		Observation prev = environment.observe();
 		double td_delta = Policy.getQ(prev.state, environment.getLastAction ());
 		environment.setObservation (observation);
+		double value = Policy.getQ(observation.state, environment.getAction ());
+		double td_target = observation.reward.getValue () + discountFactor * value;
+		td_delta = td_target - td_delta;
+		Policy.setQ(observation.state, environment.getAction (), learningRate * td_delta);
 	}
 
 	void Update(bool f, GameObject obj, int i) {
@@ -233,16 +291,17 @@ public class Controller : MonoBehaviour {
 			if (thrust [i] < 0)
 				thrust [i] = 0;
 		}
+		setText ();
 	}
+
 	public void setText() {
 		Vector3 gyroVector = new Vector3 (0, 0, 0);
 		Vector3 accelVector = new Vector3 (0, 0, 0);
-		if (observation != null) {
-			gyroVector = observation.nextState.gyro;
-			accelVector = observation.nextState.accel;
-		}
+		gyroVector = environment.observe().state.gyro;
+		accelVector = environment.observe().state.accel;
 		gyroText.text = "X = " + gyroVector[2].ToString("F3") + "\nY = " + gyroVector[0].ToString("F3") + "\nZ = " + gyroVector[1].ToString("F3");
 		accelText.text = "X = " + accelVector[0].ToString("F3") + "\nY = " + accelVector[1].ToString("F3") + "\nZ = " + accelVector[2].ToString("F3");
+		episodeText.text = episode.ToString();
 	}
 
 	void reset() {
